bbadddc26c24963c742e98e0d8f83c7e07906853
boshih <artem.bolshih@mail.ru>
2022-05-18 19:23:29 +0300

    start

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int total = 10000; // денег на счету
+        int refill = 10; // сумма пополнения
+        int bonusRubls = 0; // бонусные рубли
+
+        if (refill > 1000) {
+            bonusRubls = refill / 100;
+            total = total + refill + bonusRubls;
+        }else total = total + refill;
+        System.out.println("На счету: " + total);
+        System.out.println("Бонусных рублей: " + bonusRubls);
+    }
+}
