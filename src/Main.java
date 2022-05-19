061dd5ea986093f1274f78702b8c2e220c59d965
boshih <artem.bolshih@mail.ru>
2022-05-19 18:15:34 +0300

    доработка

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
         int total = 10000; // денег на счету
-        int refill = 10; // сумма пополнения
+        int refill = 1000; // сумма пополнения
         int bonusRubls = 0; // бонусные рубли
 
         if (refill > 1000) {
             bonusRubls = refill / 100;
             total = total + refill + bonusRubls;
-        }else total = total + refill;
+        } else {
+            total = total + refill;
+        }
         System.out.println("На счету: " + total);
         System.out.println("Бонусных рублей: " + bonusRubls);
     }
